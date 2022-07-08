049c02104006ee6a6ff82449747e9cc0db8a36e3
Filipe Vasconcelos <filipev@me.com>
2022-07-08 20:40:46 -0300

    save data

--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+def guardar_dados(info):
+    with open(f'data/{cerveja}.txt','w') as file :
+        file.write(f'{info}')
+
 def fazer_request(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text,'html.parser')
@@ -8,11 +12,13 @@ def fazer_request(url):
 
 def pegar_info(soup):
     info = soup.find('p',{'class':'textstyles__TextComponent-w4b5ef-0 ilCCve'}).text
-    print(info)
+    return info
 
 def main():
-    url='https://www.clubeextra.com.br/produto/105770/cerveja-heineken-garrafa-600ml'
-    pegar_info(fazer_request(url))
+    global cerveja
+    cerveja = 'cerveja-heineken-garrafa-600ml'
+    url=f'https://www.clubeextra.com.br/produto/105770/{cerveja}'
+    guardar_dados(pegar_info(fazer_request(url)))
 
 if __name__ == '__main__':
     main()
